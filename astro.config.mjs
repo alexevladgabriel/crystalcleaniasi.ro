ae073ab1ec078eda4b066e5e3185a812c2febccf
Alexe Vlad <alexevladgabriel@gmail.com>
2023-07-26 09:52:53 +0300

    vercel analytics:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
 import astroI18next from "astro-i18next";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +13,8 @@ export default defineConfig({
     host: true
   },
   integrations: [tailwind(), prefetch(), svelte(), image(), astroI18next()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
