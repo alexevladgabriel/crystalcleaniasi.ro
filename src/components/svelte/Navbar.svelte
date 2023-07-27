ebb07040f3bb6bd2edb0e1524a3f2d9d7eeb912c
Alexe Vlad <alexevladgabriel@gmail.com>
2023-07-27 14:38:40 +0300

    fix: translations & icons

--- src/components/svelte/Navbar.svelte
+++ src/components/svelte/Navbar.svelte
@@ -1,9 +1,10 @@
 <script>
   import clx from "classnames";
-  import Svg from "@jasikpark/astro-svg-loader";
   export let path;
   export let links;
   export let open = true;
+  export let cta = `Contactează-ne`;
+  export let phone = import.meta.env.CCI_PHONE;
 
   const handleClick = () => {
     open = !open;
@@ -57,7 +58,7 @@
       >Contactează-ne <span aria-hidden="true">&rarr;</span></a
     > -->
     <a
-      href={`tel:${import.meta.env.CCI_PHONE}`}
+      href={`tel:${phone}`}
       class="flex items-center rounded-md bg-amber-400 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-amber-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-400"
     >
       <svg class="w-4 h-4 inline-block fill-white mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -66,7 +67,7 @@
           d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"
         />
       </svg>
-      Contactează-ne <span class="ml-2" aria-hidden="true">&rarr;</span></a
+      {cta} <span class="ml-2" aria-hidden="true">&rarr;</span></a
     >
     <slot />
   </div>
